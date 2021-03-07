ce81fdbae1345887e0814bd1ce6e2ed3447cf612
Mina Naguib <mina@naguib.ca>
2021-03-06 22:39:55 -0500

    Move cat rotation state client-side

--- cat.go
+++ cat.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"sync/atomic"
+	"strings"
 )
 
 //go:embed cat1.gif
@@ -15,20 +15,30 @@ var cat1GIF []byte
 var cat2GIF []byte
 
 //go:embed cat.html
-var catHTML []byte
+var catHTML string
 
 func main() {
-
-	var catN uint32
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
 			http.NotFound(w, r)
 			return
 		}
+		http.Redirect(w, r, "/cat/1.html", http.StatusFound)
+	})
+	http.HandleFunc("/cat/1.html", func(w http.ResponseWriter, r *http.Request) {
+		html := strings.ReplaceAll(catHTML, "/cat/X.gif", "/cat/1.gif")
+		html = strings.ReplaceAll(html, "/cat/X.html", "/cat/2.html")
 		w.Header().Set("Content-Type", "text/html")
-		w.Header().Set("Content-Length", strconv.Itoa(len(catHTML)))
-		w.Write(catHTML)
+		w.Header().Set("Content-Length", strconv.Itoa(len(html)))
+		w.Write([]byte(html))
+	})
+	http.HandleFunc("/cat/2.html", func(w http.ResponseWriter, r *http.Request) {
+		html := strings.ReplaceAll(catHTML, "/cat/X.gif", "/cat/2.gif")
+		html = strings.ReplaceAll(html, "/cat/X.html", "/cat/1.html")
+		w.Header().Set("Content-Type", "text/html")
+		w.Header().Set("Content-Length", strconv.Itoa(len(html)))
+		w.Write([]byte(html))
 	})
 	http.HandleFunc("/cat/1.gif", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "image/gif")
@@ -39,11 +49,6 @@ func main() {
 		w.Header().Set("Content-Type", "image/gif")
 		w.Header().Set("Content-Length", strconv.Itoa(len(cat2GIF)))
 		w.Write(cat2GIF)
-	})
-	http.HandleFunc("/cat.gif", func(w http.ResponseWriter, r *http.Request) {
-		n := atomic.AddUint32(&catN, 1)
-		path := fmt.Sprintf("/cat/%d.gif", (n%2)+1)
-		http.Redirect(w, r, path, http.StatusFound)
 	})
 
 	fmt.Println("Starting web server at http://0.0.0.0:8081/")
